92ea9da5dcf97d375ab440c70fa01700af9b99a3
Akani Okemeka <mackyakani@gmail.com>
2023-02-16 15:25:45 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main block prints a string to standard output
+ *
+ * retuns (0) if successful
+ *
+ */
+
+int main (void)
+	{
+		printf ( "with proper grammar, but the outcome is a piece of art,"\n);
+		
+		return (0);
+	}
