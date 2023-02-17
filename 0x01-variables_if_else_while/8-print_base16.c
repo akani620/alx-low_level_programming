4b0edabcce4c8a9701c4247623061f63d4abb6b0
Akani Okemeka <mackyakani@gmail.com>
2023-02-17 14:21:59 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,20 +5,18 @@
  *Return - always 0 (success)
  *
  */
-int main(void)
+int main(void)      /*main- start of program */
 
-{   int i = '0';
-    int x = 'a';
+{int i = '0';
+ int x = 'a';
 	while (i <= '9')
 	{putchar ('i');
 		i++;
 	}
-
 	while (x <= 'f')
 	{ putchar ('x');
 		x++;
 	}
-
-    putchar ('\n');
-    return (0);
+putchar ('\n');
+return (0);
 }
