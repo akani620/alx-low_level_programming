1c3c9f5e93693b203679e66576c10fbe68d6abb7
Akani Okemeka <mackyakani@gmail.com>
2023-02-17 14:29:41 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /*
- *main - entry point
+ *main - print hexadecimal
  *
  *Return - always 0 (success)
  *
  */
 int main(void)      /*main- start of program */
-
-{int i = '0';
- int x = 'a';
+{
+int i = '0';
+char x = 'a';
 	while (i <= '9')
 	{putchar ('i');
 		i++;
