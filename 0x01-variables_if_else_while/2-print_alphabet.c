0c9272cdb9828a1121a34cf7e2a37afecdde61ee
Akani Okemeka <mackyakani@gmail.com>
2023-02-17 17:34:05 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description : print lower case alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+	while (a <= 'z')
+	{	putchar(a);
+		a++;
+	}
+putchar('\n');
+return (0);
+}
