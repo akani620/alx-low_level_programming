94b529c64ac17be5469c614bdc94a2cc5f8c9dc8
Akani Okemeka <mackyakani@gmail.com>
2023-02-21 15:44:28 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /**
- *main- entry point of program
- *
- * _isalpha- print alphabetic character
- *
- * Return: 0
- *
- *@c - argument of function
+ * _isalpha - Check if character is an alphabetic character.
+ * @c: type int character
+ * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
  */
 int _isalpha(int c)
 {
