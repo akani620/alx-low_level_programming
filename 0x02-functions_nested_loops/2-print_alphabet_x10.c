5ea691c4a230175567ffccebb59b1da65ba32289
Akani Okemeka <mackyakani@gmail.com>
2023-02-21 13:14:01 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  * Return 0
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int i=0;
 	char A;
@@ -20,5 +20,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 		i++;
 	}
-	return(0);
 }
