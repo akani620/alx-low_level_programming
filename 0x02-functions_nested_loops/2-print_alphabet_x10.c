50b9576fd387cb9e80480ad932304eb6d2d5ded8
Akani Okemeka <mackyakani@gmail.com>
2023-02-21 13:09:22 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ * Return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i=0;
+	char A;
+	while (i < 10)
+	{
+		A='a';
+		while (A <= 'z')
+		{
+			_putchar(A);
+			A++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	return(0);
+}
