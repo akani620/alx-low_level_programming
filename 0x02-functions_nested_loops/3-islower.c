4a9c064a64418021f21b663479b376f8eccbf18b
Akani Okemeka <mackyakani@gmail.com>
2023-02-21 11:08:42 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "ctype.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c);
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    putchar(r + '0');
+    r = _islower('o');
+    putchar(r + '0');
+    r = _islower(108);
+    putchar(r + '0');
+    putchar('\n');
+    return (0);
+}
+int _islower(int c)
+{
+	if ( c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
