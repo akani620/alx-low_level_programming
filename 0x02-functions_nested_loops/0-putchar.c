40dc37f3a5113b1c2110ee89da4a4cb7c534abf4
Akani Okemeka <mackyakani@gmail.com>
2023-02-21 12:27:35 -0800

    3-islower.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
-#include "main.h"
+#include <main.h>
 
 /**
- * main - Print "_putchar" followed by a new line.
- * Description: You are not allowed to include standard libraries.
- * Return: 0
+ * main - print putchar followed by a new line
+ *
+ * Return : 0
  */
-
 int main(void)
 {
-	char myVar[] = "_putchar";
+	char myvar[] = "_putchar";
 	int i = 0;
 
-	while (myVar[i] != '\0')
+	while (myvar[i] != '\0')
 	{
 		_putchar(myVar[i]);
 		i++;
 	}
 	_putchar('\n');
-
 	return (0);
 }
