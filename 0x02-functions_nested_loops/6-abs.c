dfa185d48d2075f325d36544c9b440862a2294b3
Akani Okemeka <mackyakani@gmail.com>
2023-02-21 16:33:10 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	_abs;
+
+	if (n >= 0)
+	{
+		_abs = 1*n;
+	}
+	else
+	{
+		_abs = -1*n;
+	}
+}
