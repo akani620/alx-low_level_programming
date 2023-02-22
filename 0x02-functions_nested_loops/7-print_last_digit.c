dd9db3e504d7d1288b4b8ce02a4bf5e6eee72f21
Akani Okemeka <mackyakani@gmail.com>
2023-02-21 17:13:11 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *main: program entry point
+ *
+ *Description: program to print the last digit of a number
+ *
+ *@c: argument of function
+ *
+ * return:0
+ */
+int print_last_digit(int n)
+{
+	if ( n >= 0 || n < 0)
+		return (n % 10);
+}
+
