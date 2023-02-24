2b964f3fee3d5c44fe1d4fd1786a051adfe4514e
Akani Okemeka <mackyakani@gmail.com>
2023-02-23 18:23:49 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers
+ * @a: int type number
+ * @b: int type number
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{	
+	if ((a <= 0 || a > 0) && (b <= 0 || b > 0))
+	return (a * b);
+}
