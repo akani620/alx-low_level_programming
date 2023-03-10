f5f2200575215d91fa2ecb11cd06ee0509462626
Akani Okemeka <mackyakani@gmail.com>
2023-03-10 11:39:51 -0800

    message

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,9 @@
  *main - Prints the program name, followed by a new line
  *@argc: Counter to argument of main
  *@argv: Arguments of main
+ *
  *return: Always 0
  */
-
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
