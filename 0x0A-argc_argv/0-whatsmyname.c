157c6d42d5fbd38c0235dca6531afe282dd1f0fd
Akani Okemeka <mackyakani@gmail.com>
2023-03-10 11:15:44 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main : entry point
+ *@argc : counter to argument of main
+ *@argv : arguments of main
+ *return : 0
+ */
+
+int main (int argc, char *argv[])
+{
+	printf(" %d \n",argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+argc, argv [] command line arguments in c 
+usually used to pass arguments to our programs after they have been compiled
